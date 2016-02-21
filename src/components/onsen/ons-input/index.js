c61bf5d37efcf9037d840bf8d012a739570c7c18
Artem Artemyev <aartemev@naumen.ru>
2016-02-21 15:53:03 +0500

    added ons-input text support

--- src/components/onsen/ons-input/index.js
+++ src/components/onsen/ons-input/index.js
@@ -15,23 +15,90 @@ const scheme = {
 };
 
 export default class OnsInput extends React.Component {
+
+  componentDidMount() {
+    if (this.props.type === 'text-input') {
+      this._initInputHandlers();
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    const newType = this.props.type;
+    if (prevProps.type !== newType) {
+      this._clearInputHandlers();
+      if (newType === 'text-input') {
+        this._initInputHandlers();
+      }
+    }
+  }
+
+  componentWillUnmount() {
+    this._clearInputHandlers();
+  }
+
+  _updateLabelColor() {
+    if (this._input.value.length > 0 && this._input === document.activeElement) {
+      this._helper.style.color = '';
+    }
+    else {
+      this._helper.style.color = 'rgba(0, 0, 0, 0.5)';
+    }
+  }
+
+  _updateLabelClass() {
+    if (this._input.value.trim() === '') {
+      this._helper.classList.remove('text-input__label--active');
+    }
+    else {
+      this._helper.classList.add('text-input__label--active');
+    }
+  }
+
+  _initInputHandlers = () => {
+    this._input.addEventListener('input', this._handleInputChange);
+    this._input.addEventListener('focusin', this._handleInputChange);
+    this._input.addEventListener('focusout', this._handleInputChange);
+    this._updateLabelColor();
+    this._updateLabelClass();
+  }
+
+  _clearInputHandlers = () => {
+    this._input.addEventListener('input', this._handleInputChange);
+    this._input.addEventListener('focusin', this._handleInputChange);
+    this._input.removeEventListener('focusout', this._handleInputChange);
+  }
+
+  _handleInputChange = () => {
+    this._updateLabelColor();
+    this._updateLabelClass();
+  };
+
   render() {
-    const { name, checked, onChange, type, id, ...props } = this.props;
+    const {
+      name, checked, onChange, type, id, float,
+      className, style, placeholder, value, ...props,
+    } = this.props;
+
     const labelClassName = type === 'text-input'
-      ? 'text-input__container' : type;
+      ? '' : type;
     const helperClassName = type === 'text-input'
       ? 'text-input__label' : `${type}__checkmark`;
     const inputClassName = type === 'text-input'
       ? 'text-input' : `${type}__input`;
+    const inputContainerClassName = type === 'text-input'
+      ? 'text-input__container' : '';
     const inputType = type.split('-')[0];
 
     return applyModifiers(this.props, scheme, 3,
-      <ons-input>
-        <label className={labelClassName}>
-          <span>
-            <input className={inputClassName} name={name} checked={checked}
-              id={id} onChange={onChange} type={inputType} />
-            <span className={helperClassName} />
+      <ons-input className={labelClassName} float={float}>
+        <label>
+          <span className={inputContainerClassName}>
+            <input {...props} className={inputClassName} name={name} checked={checked}
+              id={id} onChange={onChange} type={inputType} value={value}
+              ref={(d) => this._input = d} placeholder={placeholder} />
+            <span className={helperClassName} ref={(d) => this._helper = d}>
+              {placeholder}
+            </span>
           </span>
           <span className="input-label">
             {this.props.children}
